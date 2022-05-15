e71314d97d67c75cf9f22a75f34ab6dbf738f3a5
Elvina Diana <elvinadianalimartha@gmail.com>
2022-05-15 07:47:01 +0700

    minor change: move the suggestion list to suggestion class

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -186,34 +186,4 @@ class ProductService{
       throw Exception('Data produk gagal dihapus');
     }
   }
-
-  Future<List<ProductModel>> getSuggestionProduct(String query) async{
-    var url = '$baseUrl/products';
-    var headers = {
-      'Content-Type': 'application/json',
-      'Connection': 'keep-alive',
-    };
-
-    var response = await http.get(
-        Uri.parse(url),
-        headers: headers
-    );
-
-    print(response.body);
-
-    if(response.statusCode == 200) {
-      List products = jsonDecode(response.body)['data'];
-
-      return products.map((e) => ProductModel.fromJson(e)).where((user) {
-        final lowerName = user.name.toLowerCase();
-        final lowerQuery = query.toLowerCase();
-
-        return lowerName.contains(lowerQuery);
-      }).toList();
-
-      // return products;
-    } else {
-      throw Exception('Data produk gagal diambil!');
-    }
-  }
 }
