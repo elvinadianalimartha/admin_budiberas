bfdc9291ec2d4b56461b3284cd4cff0538000870
Elvina Diana <elvinadianalimartha@gmail.com>
2022-08-12 00:41:01 +0700

    add token to authenticate API access

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,16 @@
 // const String baseUrl = 'http://192.168.100.36:8000/api';
 // const String urlPhoto = 'http://192.168.100.36:8000/storage/';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 const String baseUrl = 'https://budiberas.pinulusuran.my.id/api';
 const String urlPhoto = 'https://budiberas.pinulusuran.my.id/photoProduct/';
 
 const String urlOutStock = baseUrl + '/outStock';
+
+//NOTE: get token yg tersimpan di shared preference (diset saat login)
+Future<String> getTokenAdmin() async{
+  SharedPreferences loginData = await SharedPreferences.getInstance();
+  var tokenAdmin = loginData.getString('tokenAdmin');
+  return tokenAdmin.toString();
+}
