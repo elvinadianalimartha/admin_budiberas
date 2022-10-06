3c3e47cd9348154f03ced80c28afb811d13fbf2a
Elvina Diana <elvinadianalimartha@gmail.com>
2022-10-07 04:43:52 +0700

    handling logout

--- lib/views/mainview/info_page.dart
+++ lib/views/mainview/info_page.dart
@@ -1,6 +1,13 @@
+import 'package:budiberas_admin_9701/providers/auth_provider.dart';
 import 'package:budiberas_admin_9701/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../widgets/reusable/alert_dialog.dart';
+import '../widgets/reusable/cancel_button.dart';
+import '../widgets/reusable/done_button.dart';
 
 class InfoPage extends StatefulWidget {
   const InfoPage({Key? key}) : super(key: key);
@@ -61,10 +68,54 @@ class _InfoPageState extends State<InfoPage> {
     );
   }
 
+  handleLogout() async{
+    if(await context.read<AuthProvider>().logout()) {
+      SharedPreferences loginData = await SharedPreferences.getInstance();
+      loginData.remove('tokenAdmin'); //tokenAdmin diset jd null
+      Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+    } else {
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text(
+              'Gagal logout',
+            ),
+            backgroundColor: alertColor,
+            duration: const Duration(milliseconds: 700),
+          )
+      );
+    }
+  }
+
+  Future<void> showDialogAreYouSure() async{
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialogWidget(
+          text: 'Apakah Anda yakin ingin keluar dari aplikasi?',
+          childrenList: [
+            CancelButton(
+              onClick: () {
+                Navigator.pop(context);
+              },
+              text: 'Tidak',
+              fontSize: 14,
+            ),
+            DoneButton(
+              onClick: () {
+                handleLogout();
+              },
+              text: 'Ya, keluar',
+              fontSize: 14,
+            ),
+          ],
+        )
+    );
+  }
+
   Widget logout() {
     return GestureDetector(
       onTap: () {
-        //showDialogAreYouSure();
+        showDialogAreYouSure();
       },
       child: Row(
         children: [
